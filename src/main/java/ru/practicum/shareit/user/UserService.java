6b28d9e5ba19d4343ae781334e2580309c6ac648
Dmitry Sharov <sh-wow@yandex.ru>
2026-02-10 09:30:32 +0800

    Исправление замечаний checkstyle

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public interface UserService {
     User create(User user);
+
     User update(User user);
+
     User getById(Long id);
+
     List<User> getAll();
+
     void delete(Long id);
 }
